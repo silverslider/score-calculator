e2f1e56cd215dc5f053d6aec2db9376396d9eadd
silverslider <betasindrom@hotmail.com>
2016-04-26 13:41:25 +0200

    Initalproject Branch

--- src/ch/abbts/szmg/score/calculator/ScoreCalulator.java
+++ src/ch/abbts/szmg/score/calculator/ScoreCalulator.java
@@ -15,7 +15,7 @@ public class ScoreCalulator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         // TODO code application logic
+         // Main
     }
     
 }
